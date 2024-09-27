01e405e11b25104e91d7b645efe4231d2ed3458f
Rahul <rajamohanty12345@gmail.com>
2024-09-28 03:09:42 +0530

    chore: some auth fixed

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,18 @@
-import Dashboard from "./_components/Dashboard";
+"use client";
 
+import Dashboard from "./_components/Dashboard";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 export default function Page() {
+  const router = useRouter();
+  const token = Cookies.get("accessToken");
+
+  useEffect(() => {
+    if (!token) {
+      router.push("/auth/login");
+    }
+  }, []);
+
   return <Dashboard />;
 }
