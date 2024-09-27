d8782c42f31ba537b53f262d44f3da66cdc2bd4d
Rahul <rajamohanty12345@gmail.com>
2024-09-27 22:59:54 +0530

    fix: middlware fix 5

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
-"use client";
-
-import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { Loader } from "@/components/ui/Loader";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-
-  router.push("/auth/login");
+  redirect("/auth/login");
 
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+    <div className="absolute inset-0 flex items-center justify-center h-full w-full">
+      <Loader />
     </div>
   );
 }
